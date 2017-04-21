123d754550beb08bfd00673ddb951be9feb7f021
roycepo <roycepo.framgia@gmail.com>
2017-04-21 18:21:36 +0800

    Displaying The Tasks

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -3,29 +3,57 @@
 @section('content')
 
 
-	<div class ="panel-body">
+    <div class ="panel-body">
 
-	   @include('common.errors')
+       @include('common.errors')
 
-	   <form action ="{{ url('task)" }}" method="POST" class="form-horizontal">
-	       {{ csrf_field() }}
+       <form action ="{{ url('task') }}" method="POST" class="form-horizontal">
+           {{ csrf_field() }}
 
-	       <div class="form-group">
-	        	<label for="task-name" class="col-sm-3 control-label">Task</label>
+           <div class="form-group">
+                <label for="task-name" class="col-sm-3 control-label">Task</label>
 
-	        	<div class="col-sm-6">
-	        		<input type="text" name="name" id="task-name" class="form-control">
-	        	</div>
-	        </div>
+                <div class="col-sm-6">
+                    <input type="text" name="name" id="task-name" class="form-control">
+                </div>
+            </div>
 
-	        <div class="form-group">
-	        	<div class="col-sm-offset-3 col-sm-6">
-	        		<button type="submit" class="btn btn-default">
-	        			<i class="fa fa-plus"></i>Add Task
-	        		</button>
-	        	</div>
-	        </div>
-	   </form>
-	</div>
-	<!-- TODO: Current Tasks -->
+            <div class="form-group">
+                <div class="col-sm-offset-3 col-sm-6">
+                    <button type="submit" class="btn btn-default">
+                        <i class="fa fa-plus"></i>Add Task
+                    </button>
+                </div>
+            </div>
+       </form>
+    </div>
+    <!-- TODO: Current Tasks -->
+    @if (count($tasks) >0)
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Current Tasks
+            </div>
+        <div class="panel-body">
+            <table class="table table-striped task-table">
+                <thead>
+                    <th>Task</th>
+                    <th>$nbsp;</th>
+                </thead>
+
+                <tbody>
+                    @foreach ($tasks as $task)
+                        <tr>
+                            <td class="table-text">
+                                <div> {{ $task->name}} </div>
+                            </td>
+                            <td>
+                                /* Delete Button */
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+    @endif
 @endsection
