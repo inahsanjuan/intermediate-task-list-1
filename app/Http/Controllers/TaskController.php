3f31f285a2afcd9fc482a7f984041f7d692a997f
roycepo <roycepo.framgia@gmail.com>
2017-04-21 17:57:52 +0800

    Dependency Injection

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,19 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Task;
+use App\Repositories\TaskRepository;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class TaskController extends Controller
 {
+    /* Task Repository Instance */
+    protected $tasks;
+
+    /* Creates a new controller instance. */
     public function __construct()
     {
     	$this->middleware('auth');
+
+        $this->tasks = $tasks;
     }
 
+    /* Displays a list of all the user tasks */
     public function index(Request $request)
     {
-    	return view('tasks.index');
+    	$tasks = $request->user()->tasks()->get();
+
+        return view('tasks.index', [
+            'tasks' => $tasks->tasks->forUser($request->user()),
+            ]);
     }
 
     public function store(Request $request)
